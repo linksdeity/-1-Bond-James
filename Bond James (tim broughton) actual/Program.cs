39fa0f29b969757320c93f63bf873c4c822996ed
Tim Broughton <TimBroughton@quickenloans.com>
2018-10-12 15:52:26 -0400

    initial commit

--- Bond James (tim broughton) actual/Program.cs
+++ Bond James (tim broughton) actual/Program.cs
@@ -10,6 +10,23 @@ namespace Bond_James__tim_broughton__actual
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter your first and last name:");
+
+            string userInput = Console.ReadLine();
+
+            string[] firstAndLast = userInput.Split(null);
+
+            string firstName = firstAndLast[0];
+
+            string lastName = firstAndLast[1];
+
+            Console.WriteLine("{0} {1}", lastName, firstName);
+
+
+
+            Console.ReadKey();
         }
+
+        
     }
 }
